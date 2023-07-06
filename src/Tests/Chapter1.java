f5d1c55756649f7aa11b6728ca0e20ee29c84590
Li Shaojie <shaojie.li199@gmail.com>
2023-07-06 15:46:43 +0200

    完成基础练习

--- src/Tests/Chapter1.java
+++ src/Tests/Chapter1.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Chapter1 {
@@ -23,7 +25,14 @@ public class Chapter1 {
         // test1_1_13(3,2);
         // test1_1_14(2);
         // test1_1_15(10);
-        test1_1_16();
+        // test1_1_16();
+        // test1_1_18();
+        // test1_1_19();
+        // test1_1_20();
+        // test1_1_21(2);
+        // test1_1_22();
+        // test1_1_23(args);
+        test1_1_24();
     }
 
     static void test1_1_1(){
@@ -262,6 +271,157 @@ public class Chapter1 {
         return exR1(n-3) + n + exR1(n-2) + n;
     }
 
-    
+    static void test1_1_18(){
+        System.out.println(mystery2(3,11));
+         /**
+          * mystory
+          * a=2
+          * b=25     25 12 6  3  1
+          *          1  0  0  1  1
+          *          2  4  8  16 32
+          * =2+16+32=50
+          *
+          * a=3
+          * b=11  11 5 2  1
+          *       1  1 0  1
+          *       3  6 12 24
+          * =3+6+24=33   
+          *
+          * mystory2
+          * a=2
+          * b=25 25 12 6  3   1
+          *      1  0  0  1   1
+          *      2  4  16 256 65536    
+          * =2*256*65536=33554432
+          *
+          * a=3
+          * b=11 11 5 2  1
+          *      1  1 0  1
+          *      3  9 81 6561
+          * =3*9*6561=177147
+          */ 
+    }
+
+    static int mystery(int a, int b){
+        if (b == 0) return 0;
+        if (b % 2 ==0 ) return mystery(a+a, b/2);
+        return mystery(a+a, b/2) + a;
+    }
+
+    static int mystery2(int a, int b){
+        if (b == 0) return 1;
+        if (b % 2 ==0 ) return mystery2(a*a, b/2);
+        return mystery2(a*a, b/2) * a;
+    }
+
+    static void test1_1_19(){
+        long[] result = new long[100];
+        for (int N = 0; N < 100; N++){
+            // System.out.println(N + " " + F(N));
+            result[N] = F2(N, result);
+            System.out.println(N + " " + result[N]);
+        }
+    }
+
+    static long F(int N){
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        return F(N-1) + F(N-2);
+    }
+
+    static long F2(int N, long[] result){
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        return result[N-1] + result[N-2];
+    }
+
+    static void test1_1_20(){
+        System.out.println(ln(3));
+    }
+
+    static long ln(long n){
+        if (n == 1) return 1;
+        else return n*ln(n-1);
+    }
+
+    static void test1_1_21(int N){
+        String[] name = new String[N];
+        int[] a = new int[N];
+        int[] b = new int[N];
+        int index = 0;
+        while (!StdIn.isEmpty() && index < N){
+            name[index] = StdIn.readString();
+            a[index] = StdIn.readInt();
+            b[index] = StdIn.readInt(); 
+            index++;
+        }
+        for (int i = 0; i < index; i++){
+            System.out.printf("%s %d %d %.3f\n", name[i], a[i], b[i], (double)a[i]/b[i]);
+        }
+    }
+
+    static void test1_1_22(){
+        int [] array = new int[StdRandom.uniformInt(10, 100)];
+        for (int i = 0; i < array.length; i++){
+            array[i] = StdRandom.uniformInt(1000);
+        }
+        Arrays.sort(array);
+
+        int key = StdRandom.uniformInt(1000);
+
+        rank(key, array );
+    }
+
+    static int rank(int key, int[] a){
+        int deep = 0;
+        return rank(key, a, 0, a.length - 1, deep);
+    }
+
+    static int rank(int key, int[] a, int lo, int hi, int deep){
+        
+        if (lo > hi) return -1;
+        int mid = lo + (hi - lo) / 2;
+        // System.out.printf("lo = %d, hi = %d, mid = %d deep = %d\n",lo, hi, mid, ++deep);
+        if (key < a[mid])       return rank(key, a, lo, mid - 1, deep);
+        else if (key > a[mid])  return rank(key, a, mid + 1, hi, deep);
+        else                    return mid;
+    }
+
+    static void test1_1_23(String[] args){
+        if (args.length < 2){
+            return;
+        }
+        int[] whitelist = new In(args[0]).readAllInts();
+        String display_control = args[1];
+        Arrays.sort(whitelist);
+        while (! StdIn.isEmpty()){
+            int key = StdIn.readInt();
+            if (display_control.equals("+")){
+                if(rank(key, whitelist) < 0){
+                    System.out.println(key);
+                }  
+            }else if(display_control.equals("-")){
+                if(rank(key, whitelist) > 0){
+                    System.out.println(key);
+                }  
+            }
+        }
+    }
+
+    static void test1_1_24(){
+        System.out.println(gcd(32,2));
+    }
+
+    static int gcd(int a, int b){
+        /**gcd(a,b) = gcd(b,a%b)
+         *
+         * https://www.bilibili.com/read/cv18449834/
+         * https://juejin.cn/post/7140890852573839391
+         * https://zhuanlan.zhihu.com/p/42707457
+         * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+          */
+        if(a % b == 0) return b;
+        else return gcd(b, a % b);
+    }
 
 }
